756e34a5cdc1c3dc1f991bd5edc2ad86d7955287
Alex Burla <sashaburla24@gmail.com>
2024-08-12 21:45:43 +0300

    used dj_database_url to setup the default db

--- centum_d_test/settings.py
+++ centum_d_test/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+
+import dj_database_url
 import environ
 
 
@@ -100,10 +102,9 @@ WSGI_APPLICATION = 'centum_d_test.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgres://localhost'
+    )
 }
 
 
