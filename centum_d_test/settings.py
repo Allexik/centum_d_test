b71de5540033bf7aa0d1613a011a3dafeb021c1c
Alex Burla <sashaburla24@gmail.com>
2024-08-13 17:26:43 +0300

    added and used 'Open Sans' font

--- centum_d_test/settings.py
+++ centum_d_test/settings.py
@@ -148,6 +148,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
